c19b76607f8434f9bc3a9aa04b1e106d41805956
zhabwe254 <zhabwe@gmail.com>
2023-02-16 10:09:35 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
-#Include <stdio.h>
-
+#Include < stdio.h >
+/**
+ * main - puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0); }
