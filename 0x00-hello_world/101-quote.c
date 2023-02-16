156fbd5dc7d3b8883534497e4b83de2b680443e3
zhabwe254 <zhabwe@gmail.com>
2023-02-16 14:50:16 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unlistd.h>
+/**
+ * main - A C program that prints a line to the standard error
+ * Return: 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
