5b00e14d3a8485b4064120a06e45aa64775a8cef
zhabwe254 <zhabwe@gmail.com>
2023-02-16 10:45:47 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #Include < stdio.h >
 /**
- * main - puts function
+ * main - print line using  puts function
+ *
  * Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"
-n);
-return (0); }
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
