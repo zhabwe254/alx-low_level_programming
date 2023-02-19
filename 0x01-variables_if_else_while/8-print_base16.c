b027df8fa66fd1e0c3c5bb0ebb8bf8903120e5f0
zhabwe254 <zhabwe@gmail.com>
2023-02-19 04:51:50 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return:
+ */
+int main(void)
+{
+	char c;
+	
+	int d;
+
+	c = 'a';
+	d = '0';
+	while
+		(d <= 10) }
+			putchar(d + '0');
+			c++;
+			}
+	while
+		(c <= 'f') }
+			putchar(c);
+			c++;
+			}
+putchar('\n');
+return (0);
+}
