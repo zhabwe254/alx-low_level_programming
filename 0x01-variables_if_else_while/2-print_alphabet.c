aadadf9753a3a6aef687184b9eff6a37c80d6fe7
zhabwe254 <zhabwe@gmail.com>
2023-02-19 03:14:39 -0800

    n2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{char c;
+
+	c = "a";
+	whie
+		(c <= "z") {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
