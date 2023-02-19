dd16ad016c05bdc0315606fb19a5d2650b47c400
zhabwe254 <zhabwe@gmail.com>
2023-02-19 05:08:19 -0800

    ./8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,16 +2,17 @@
 /**
  * main - Entry point
  *
- * Return: 0
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
 	char c;
+	
 	int d;
 
 	c = 'a';
-	d = '0';
+	d = 0;
 	while
 		(d < 10) {
 			putchar(d + '0');
