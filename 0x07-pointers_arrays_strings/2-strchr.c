bf270a78446b6beadb78c6cc619d6d4ec09bc3ea
zhabwe254 <zhabwe@gmail.com>
2023-03-06 11:13:52 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,18 +12,18 @@
 
 char *_strchr(char *s, char c)
 {
-	if (s == NILL)
+	if (s == NULL)
 	{
 	return (NULL);
 	}
 
 	while (*s != '\0')
 	{
-		if (*s == c)
-		{
-		return (s);
-		}
-		s++;
+	if (*s == c)
+	{
+	return (s);
+	}
+	s++;
 	}
 	if (*s == '\0' && c == '\0')
 	{
