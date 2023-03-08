9e38c1960d3429a7b891ea24d1a179f7a91b7460
zhabwe254 <zhabwe@gmail.com>
2023-03-08 09:49:09 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -13,13 +13,21 @@ int is_prime_number(int n)
 
 	if (n <= 1)
 		return (0);
-	if (n <= 3)
+	if (n == 2 || n == 3)
 		return (1);
 
-	for (i = 2; i * i <= n; i++)
+/**
+ * Loop from 2 up to n/2 and check if n is divisible by any of those numbers.
+ * If n is divisible, it is not a prime number and we can
+ *
+ * Return 0 if n is divisible, thus it is not a prime number.
+ */
+
+	for (i = 2; i <= n / 2; i++)
 	{
 		if (n % i == 0)
 			return (0);
 	}
+
 	return (1);
 }
