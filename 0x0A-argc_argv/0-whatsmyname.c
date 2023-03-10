a96ece530582322994872bdba74b1a8df789ddba
zhabwe254 <zhabwe@gmail.com>
2023-03-10 10:47:44 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,9 @@
  * Return: 0
  */
 
-int main(int argc, char **argv);
+int main(int argc __attribute__((__unused__)), char *argv[])
 {
-	(void)argc;
+	printf("%s\n", *argv);
 
-	printf("%s\n", argv[0]);
 	return (0);
 }
