13895185450107a62b126314c3fe011ac9718f31
zhabwe254 <zhabwe@gmail.com>
2023-09-27 09:02:42 -0700

    A

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,34 +1,34 @@
 #include "main.h"
 
 /**
- * str_similarity_checker - Determine if two strings are identical or similar.
+ * str_checker - Determine if two strings are identical or similar.
  * @s1: Base address of the first string.
  * @s2: Base address of the second string.
  * @i: Left index for the first string.
  * @j: Special index (joker) for the second string.
  * Return: 1 if the strings are identical or similar, 0 otherwise.
  */
-int str_similarity_checker(char *s1, char *s2, int i, int j)
+int str_checker(char *s1, char *s2, int i, int j)
 {
 	if (s1[i] == '\0' && s2[j] == '\0')
 		return (1);
 	if (s1[i] == s2[j])
-		return (str_similarity_checker(s1, s2, i + 1, j + 1));
+		return (str_checker(s1, s2, i + 1, j + 1));
 	if (s1[i] == '\0' && s2[j] == '*')
-		return (str_similarity_checker(s1, s2, i, j + 1));
+		return (str_checker(s1, s2, i, j + 1));
 	if (s2[j] == '*')
-		return (str_similarity_checker(s1, s2, i + 1, j) || str_similarity_checker(s1, s2, i, j + 1));
+		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
 	return (0);
 }
 
 /**
- * wildcmp - Check if two strings can be considered identical or similar.
+ * wildcmp - two strings can be considered identical or similar.
  * @s1: Base address of the first string.
  * @s2: Base address of the second string.
  *
- * Return: 1 if the strings are considered identical or similar, 0 otherwise.
+ * Return: 1 if the strings are identical or similar, 0 otherwise.
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (str_similarity_checker(s1, s2, 0, 0));
+	return (str_checker(s1, s2, 0, 0));
 }
