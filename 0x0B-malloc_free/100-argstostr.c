21fae8dad36a8ace3c88d2af719f8bd97eb75095
zhabwe254 <zhabwe@gmail.com>
2023-10-04 02:44:11 +0300

    a

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,40 +2,42 @@
 #include <stdlib.h>
 
 /**
- * argstostr - Concatenates all the arguments of the program.
- * @ac: The number of arguments.
- * @av: An array of pointers to the arguments.
- *
- * Return: A pointer to the concatenated string, or NULL if memory
- *         allocation fails or ac is 0.
+ * argstostr - Concatenate all arguments of the program.
+ * @ac: Number of arguments.
+ * @av: Array of arguments.
+ * Return: A pointer to the concatenated string or NULL if it fails.
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, len = 0, total_len = 0;
+	int i, n, k = 0, len = 0;
 	char *str;
 
 	if (ac == 0 || av == NULL)
-		return (NULL);
+	return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j]; j++)
-			len++;
-
-		total_len += len + 1;
-		len = 0;
+		for (n = 0; av[i][n]; n++)
+		len++;
 	}
+	len += ac;
 
-	str = malloc(total_len * sizeof(char));
+	str = malloc(sizeof(char) * (len + 1));
 	if (str == NULL)
-		return (NULL);
+	return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j]; j++)
-			str[len++] = av[i][j];
-		str[len++] = '\n';
+	for (n = 0; av[i][n]; n++)
+	{
+		str[k] = av[i][n];
+			k++;
 	}
-	str[total_len - 1] = '\0';
+	if (str[k] == '\0')
+	{
+		str[k++] = '\n';
+	}
+	}
 	return (str);
 }
+
