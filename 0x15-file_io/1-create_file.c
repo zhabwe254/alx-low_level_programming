201a025fbb050b20688bdbae3db7e3f5fb159e6b
zhabwe254 <zhabwe@gmail.com>
2023-11-02 00:30:11 +0300

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,39 @@
-#include <fcntl.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
- * create_file - Creates a file with specific permissions and writes content.
- * @filename: Name of the file to create.
- * @text_content: NULL-terminated string to write to the file.
+ * create_file - creates a file
+ * @filename: The name of the file to be created.
+ * @text_content: The content to be written into the file.
  *
- * Return: 1 on success, -1 on failure (file cannot be created, written, etc.).
+ * Return: 1 on success, -1 on failure.
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
-	return (-1);
+	if (!filename)
+		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
-	return (-1);
+		return (-1);
 
-	if (text_content != NULL)
-	{
-		bytes_written = write(fd, text_content, strlen(text_content));
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
 
 	close(fd);
+
 	return (1);
-}
+
