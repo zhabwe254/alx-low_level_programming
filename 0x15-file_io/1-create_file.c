5a9170bfbaa6170c51ce37102da2bc208c7f91fb
zhabwe254 <zhabwe@gmail.com>
2023-11-02 00:35:51 +0300

    A

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
- * @filename: The name of the file to be created.
- * @text_content: The content to be written into the file.
+ * create_file - Creates a new file or truncates an existing file.
+ * @filename: The name of the file to be created or truncated.
+ * @text_content: The content to be written to the file.
  *
  * Return: 1 on success, -1 on failure.
  */
@@ -36,4 +36,4 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 
 	return (1);
-
+}
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,42 +1,13 @@
 #include "main.h"
 
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: filename.
- * @text_content: added content.
+ * append_text_to_file - Appends text to the end of a file.
+ * @filename: The name of the target file.
+ * @text_content: The content to be added.
  *
- * Return: 1 if the file exists. -1 if the fails does not exist
- * or if it fails.
+ * Return: 1 if the file exists, -1 if nonexistent or if the operation fails.
  */
-int append_text_to_file(const char *filename, char *text_content)
-{
-	int fd;
-	int nletters;
-	int rwr;
 
-	if (!filename)
-		return (-1);
-
-	fd = open(filename, O_WRONLY | O_APPEND);
-
-	if (fd == -1)
-		return (-1);
-
-	if (text_content)
-	{
-		for (nletters = 0; text_content[nletters]; nletters++)
-			;
-
-		rwr = write(fd, text_content, nletters);
-
-		if (rwr == -1)
-			return (-1);
-	}
-
-	close(fd);
-
-	return (1);
-}
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
