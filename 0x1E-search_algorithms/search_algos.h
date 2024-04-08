af0791139e9d1164d75b5ac8b73fdd398e198437
zhabwe254 <zhabwe@gmail.com>
2024-04-09 02:59:59 +0300

    a

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,10 +3,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* Function prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t start, size_t end);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif /* SEARCH_ALGOS_H */
