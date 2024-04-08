98eb28da1c4ef07c65ff84280c7c588bc6055602
zhabwe254 <zhabwe@gmail.com>
2024-04-09 02:05:30 +0300

    a

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t start, size_t end);
+
+#endif /* SEARCH_ALGOS_H */
